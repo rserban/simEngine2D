7415ae907113b8e64021f403501c79c9206602db
Radu Serban <serban@engr.wisc.edu>
2013-10-31 08:36:44 -0500

    Cleanup.

--- Assignment_6_1.m
+++ Assignment_6_1.m
@@ -1,25 +1,47 @@
 function [] = Assignment_6_1()
+% Sample program for solving the system of nonlinear equations:
+%    3x + sin(xy) - 4 = 0
+%    x - exp(cos(y)) = 0 
+% using Newton's method, with a tolerance 1e-8, using as an initial guess
+%    x0 = 1.5
+%    y0 = 1.5
+%
 
+% Exact solution:
+q_an = [1; pi/2];
+
+% Specify maximum number of N-R iterations and stopping tolerance
 max_it = 20;
 tol = 1e-8;
 
 % Initial guess
 q = [1.5; 1.5];
 
+fprintf('Initial guess q = [%20.16f %20.16f]  ||error|| = %20.16f\n', ...
+        q, norm(q - q_an));
+
 % Perform at most max_it N-R iterations
 for it = 1:max_it
+    % Calculate residual and Jacobian at current iterate
     res = calcPhi(q);
     jac = calcJac(q);
+    
+    % Calculate the correction
     corr = jac\res;
-    q = q - corr;
     norm_corr = norm(corr);
+    q = q - corr;   
     
-    fprintf('Iteration: %i  ||correction|| = %e  q = [%f  %f]\n', it, norm_corr, q);
-    
+    % Output information at current iteration
+    fprintf('Iteration: %i  q = [%20.16f %20.16f]  ||error|| = %20.16f',...
+            it, q,  norm(q - q_an));
+    fprintf('   ||correction|| = %e\n', norm_corr);
+        
+    % Stop if the norm of the correction is below the specified tolerance.
     if norm_corr <= tol
         break;
     end
 end
+
 
 end
 
--- Assignment_6_2.m
+++ Assignment_6_2.m
@@ -5,18 +5,26 @@ sys = MBsys('Models/simplePend.adm');
 sys.print();
 
 % At t = 0
-fprintf('At initial time:\n');
+fprintf('\n\nAt initial time:\n');
 [q0,qd0] = sys.setIC;
-Phi = sys.evalPhi(0, q0)
-Jac = sys.evalPhi_q(0, q0)
-nu = sys.evalNu(0, q0)
-gamma = sys.evalGamma(0, q0, qd0)
+Phi = sys.evalPhi(0, q0);
+Jac = sys.evalPhi_q(0, q0);
+nu = sys.evalNu(0, q0);
+gamma = sys.evalGamma(0, q0, qd0);
+
+fprintf('Phi   = [%10.6f\n         %10.6f\n         %10.6f]\n', Phi);
+fprintf('Phi_q = [%10.6f  %10.6f  %10.6f]\n', Jac(1,:));
+fprintf('        [%10.6f  %10.6f  %10.6f]\n', Jac(2,:));
+fprintf('        [%10.6f  %10.6f  %10.6f]\n', Jac(3,:));
+fprintf('nu    = [%10.6f\n         %10.6f\n         %10.6f]\n', nu);
+fprintf('gamma = [%10.6f\n         %10.6f\n         %10.6f]\n', gamma);
 
 
 % Loop on a time grid...
 dt = 0.01;
 nt = 100;
 
+fprintf('---------------------------\n');
 fprintf('At t = 0.37\n');
 for it = 1:nt
     t = dt*it;
@@ -29,9 +37,18 @@ for it = 1:nt
     Phi = sys.evalPhi(t, q);
     Jac = sys.evalPhi_q(t, q);
     nu = sys.evalNu(t, q);
+    qd = Jac\nu;
+    gamma = sys.evalGamma(0, q, qd);
+    qdd = Jac\gamma;
     if it == 37
-       disp(Phi)
-       disp(Jac)
-       disp(nu)
+        fprintf('q     = [%10.6f\n         %10.6f\n         %10.6f]\n', q);
+        fprintf('qd    = [%10.6f\n         %10.6f\n         %10.6f]\n', qd);
+        fprintf('qdd   = [%10.6f\n         %10.6f\n         %10.6f]\n', qdd);
+        fprintf('Phi   = [%10.6f\n         %10.6f\n         %10.6f]\n', Phi);
+        fprintf('Phi_q = [%10.6f  %10.6f  %10.6f]\n', Jac(1,:));
+        fprintf('        [%10.6f  %10.6f  %10.6f]\n', Jac(2,:));
+        fprintf('        [%10.6f  %10.6f  %10.6f]\n', Jac(3,:));
+        fprintf('nu    = [%10.6f\n         %10.6f\n         %10.6f]\n', nu);
+        fprintf('gamma = [%10.6f\n         %10.6f\n         %10.6f]\n', gamma);
     end
 end
