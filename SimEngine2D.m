3be2c5d6f20dfd6915dca2e421a1258a904bcb3d
Radu Serban <serban@engr.wisc.edu>
2013-11-26 09:59:22 -0600

    Add timing.

--- SimEngine2D.m
+++ SimEngine2D.m
@@ -12,16 +12,20 @@ function [sys, data] = SimEngine2D(model_file, analysis_file)
 %   See also MBSYS, KINEMATICS, DYNAMICS
 
 %% Create the MBsystem using the definition in the ADM file.
+tic;
 sys = MBsys(model_file);
+fprintf('Time to read model: %f\n', toc);
 
 %% Parse the ACF file to extract analysis information.
 an = loadjson(analysis_file);
 
+tic;
 if strcmpi(an.simulation, 'kinematics')
     data = kinematics(sys, 0, an.tend, an.stepSize, an.outputSteps);
 elseif strcmpi(an.simulation, 'dynamics')
     data = dynamics(sys, 0, an.tend, an.stepSize, an.outputSteps);
 end
+fprintf('time for analysis: %f\n', toc);
 
 %% Postprocessing of simulation data...
 vis = input('Animate? Y/N [Y]:', 's');
